5cd8967a7844d8f5ec7a4ba2fe7b7faa4bf501fb
Jason Casiday <jason@Jasons-MacBook-Pro-2.local>
2022-12-02 08:14:18 -0500

    d01p02

---
+++ d01p02.js
@@ -0,0 +1,20 @@
+require('fs').readFile('d01p01.txt', (err, file) => {
+
+    const mostCalories =
+        // load from file and group by blank line
+        file.toString().split('\n').reduce((prev, item) => {
+            item === '' ? prev.push([]) : prev[prev.length - 1].push(Number(item));
+            return prev;
+        }, [[]]).filter(a => a.length) // drop empty array
+            // sum arrays
+            .map(g => g.reduce((prev, item) => prev + item))
+            // sort descending
+            .sort((a, b) => b - a)
+            // grab first 3
+            .slice(0, 3)
+            // sum
+            .reduce((prev, item) => prev + item);
+
+    console.log('Most calories: ' + mostCalories);
+
+});
